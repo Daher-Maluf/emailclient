9a9723e4506a2f19357b0af935765a946b21fa92
fausto <faustomalufff@gmail.com>
2020-11-02 13:46:58 -0300

    added email reply functionality

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnChanges, Input } from '@angular/core';
 import { Email } from '../email';
+import {EmailService} from '../email.service';
 
 @Component({
   selector: 'app-email-reply',
   templateUrl: './email-reply.component.html',
   styleUrls: ['./email-reply.component.css']
 })
-export class EmailReplyComponent  {
+export class EmailReplyComponent  implements OnChanges {
   showModal = false;
   @Input() email: Email;
 
-  constructor() { }
+  constructor(private emailService: EmailService) { }
 
   ngOnChanges(): void {
     const text = this.email.text.replace(/\n/gi, '\n> ')
@@ -24,7 +25,9 @@ export class EmailReplyComponent  {
   }
 
   onSubmit(email: Email) {
-
+     this.emailService.sendEmail(email).subscribe(() => {
+       this.showModal = false;
+     })
   }
 
 }
